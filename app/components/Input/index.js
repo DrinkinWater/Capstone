0e429b6add15d20e490e3eac86974039417d2924
jeffhappily <goodboy96@live.com.my>
2017-11-12 17:05:09 +0800

    Add textarea to sos screen:

--- app/components/Input/index.js
+++ app/components/Input/index.js
@@ -1,4 +1,5 @@
 import SearchBar from './SearchBar'
 import FormInput from './FormInput'
+import Textarea from './Textarea'
 
-export { SearchBar, FormInput }
+export { SearchBar, FormInput, Textarea }
--- app/config/api.js
+++ app/config/api.js
@@ -1,4 +1,3 @@
-export const API_ROOT = "http://10.0.2.2:8080"
-// export const API_ROOT = "https://capstoneapi.herokuapp.com"
+// export const API_ROOT = "http://10.0.2.2:8080"
+export const API_ROOT = "https://capstoneapi.herokuapp.com"
 // export const API_ROOT = "https://awubzddytg.localtunnel.me"
-
